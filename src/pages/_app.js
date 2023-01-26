bc418c0c19a812918f316a0cd232cdd4e2210a47
d-lite <olabemiwodaniel@gmail.com>
2023-01-26 12:50:05 +0100

    fix script issue with vercel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import '@/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import Head from "next/head";
-import Script from "next/script";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { useEffect } from "react";
 
@@ -20,16 +19,6 @@ export default function App({ Component, pageProps }) {
   <>
     <Head>
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
-
-
-            <Script src="assets/js/jquery.min.js" />
-            <Script src="assets/js/popper.min.js" />
-            <Script src="assets/js/bootstrap.min.js" />
-            <Script src="assets/js/multislider.js" />
-
-
-
-
     </Head>
     <Component {...pageProps} />
   </>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -8,6 +9,10 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+        <Script src="assets/js/jquery.min.js" />
+        <Script src="assets/js/popper.min.js" />
+        <Script src="assets/js/bootstrap.min.js" />
+        <Script src="assets/js/multislider.js" />
     </Html>
   )
 }
